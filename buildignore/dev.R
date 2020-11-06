50bf22f6c9e69f9eb1c2983cc78a04ed6ee87202
eokodie <eokodie@gmail.com>
2020-11-06 19:16:28 +0000

    Edit dev script

--- buildignore/dev.R
+++ buildignore/dev.R
@@ -20,26 +20,3 @@ get_historic_quotes(
 )
 
 
-############
-# checks
-if(!is.character(token)) stop("token must be a character")
-if(!is.character(ticker)) stop("ticker must be a character")
-if(!is.character(date)) stop("date must be a character")
-
-# construct endpoint
-url <- "https://api.polygon.io"
-url <- glue::glue(
-  "{url}/v2/ticks/stocks/nbbo/{ticker}/{date}"
-)
-url <- httr::modify_url(
-  url,
-  query = list(
-    apiKey = token
-  )
-)
-
-# get response
-response <- httr::GET(url)
-content <- httr::content(response, "text", encoding = "UTF-8")
-content <- jsonlite::fromJSON(content)
-if(content$status == "ERROR") stop(content$error)
